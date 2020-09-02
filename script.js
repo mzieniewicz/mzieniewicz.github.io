6e3606e7cc511cb800ade89998e1b08c179fd277
mzieniewicz <mzieniewicz@gmail.com>
2020-09-02 22:25:25 +0200

    add Trello synchronization and bug fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://glacial-castle-64371.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'http://glacial-castle-64371.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
